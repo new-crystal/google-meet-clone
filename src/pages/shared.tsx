57f1ea5fecea3ee4fb021c68e050bdea789f76be
hakyunglee <aszx601@naver.com>
2022-12-21 16:42:21 +0900

    initial commit

--- src/pages/shared.tsx
+++ src/pages/shared.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./main/Main";
+import Join from "./join/Join";
 
 const Shared = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/meeting" />
-      <Route path="/join" />
+      <Route path="/join" element={<Join />}/>
       <Route path="*" element={<Main />} />
     </Routes>
   );
