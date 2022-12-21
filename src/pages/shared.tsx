46d63cc483738f6fb183141e8780f8fcc2fae33e
hakyunglee <aszx601@naver.com>
2022-12-21 22:56:55 +0900

    [ADD]조인페이지 route 수정

--- src/pages/shared.tsx
+++ src/pages/shared.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./main/Main";
-import JoinNotLogIn from "./join/JoinNotLogin";
 import Join from "./join/Join";
 
 const Shared = () => {
@@ -10,7 +9,6 @@ const Shared = () => {
       <Route path="/" element={<Main />} />
       <Route path="/meeting" />
       <Route path="/join" element={<Join />}/>
-      <Route path="/joinnotlogin" element={<JoinNotLogIn />}/>
       <Route path="*" element={<Main />} />
     </Routes>
   );
