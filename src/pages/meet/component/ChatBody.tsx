147f49b4ee6ae6ae561e2630f723ea8fc0b99a6e
crystal025 <oj3339@naver.com>
2022-12-22 18:09:22 +0900

    #26[ADD]채팅읽기

--- src/pages/meet/component/ChatBody.tsx
+++ src/pages/meet/component/ChatBody.tsx
@@ -1,13 +1,12 @@
 import { async } from "@firebase/util";
 import { collection, getDocs } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { db } from "../../../api/firebase";
 
 const ChatBody = () => {
-  const [chatData, setChatData] = useState([
-    { chatTime: "오후 4:58", content: "ddddd", userNick: "susu" },
-  ]);
+  const [chatData, setChatData] = useState<ChatData>([]);
+  const scrollRef = useRef<HTMLDivElement>(null);
 
   interface Chat {
     chatTime: string;
@@ -18,25 +17,43 @@ const ChatBody = () => {
   interface ChatData extends Array<Chat> {}
 
   const getChatList = async () => {
+    let chats: Chat[] = [];
     const chatList = await getDocs(collection(db, "chat"));
     chatList.forEach((chat): void => {
-      console.log(chat.data());
+      let chatTime = chat.data().chatTime;
+      let content = chat.data().content;
+      let userNick = chat.data().userNick;
+      chats.push({ chatTime, content, userNick });
+      setChatData(chats);
     });
   };
 
+  const scrollToBottom = useCallback(() => {
+    scrollRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+  }, [chatData]);
+
   useEffect(() => {
     getChatList();
+    scrollToBottom();
   }, []);
 
   return (
     <STChatBodyBox>
-      <STChat>
-        <STChatNickTimeBox>
-          <STChatNick></STChatNick>
-          <STChatTime></STChatTime>
-        </STChatNickTimeBox>
-        <STChatContent></STChatContent>
-      </STChat>
+      {chatData.map((chat: Chat) => {
+        return (
+          <STChat ref={scrollRef}>
+            <STChatNickTimeBox>
+              <STChatNick>{chat.userNick}</STChatNick>
+              <STChatTime>{chat.chatTime}</STChatTime>
+            </STChatNickTimeBox>
+            <STChatContent>{chat.content}</STChatContent>
+          </STChat>
+        );
+      })}
     </STChatBodyBox>
   );
 };
@@ -58,20 +75,16 @@ const STChatBodyBox = styled.div`
 const STChat = styled.div`
   display: flex;
   padding: 0.75rem 1.5rem 0.875rem;
-  align-items: center;
   justify-content: center;
   flex-direction: column;
 `;
 const STChatNickTimeBox = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: row;
 `;
 const STChatNick = styled.div`
   color: #202124;
   display: inline-block;
-  float: left;
   font-size: 0.8125rem;
   font-weight: 500;
   line-height: 1.25;
@@ -83,13 +96,11 @@ const STChatNick = styled.div`
 const STChatTime = styled.div`
   color: #5f6368;
   display: inline-block;
-  float: left;
   font-size: 0.75rem;
   line-height: 1.5;
   padding-top: 0.1875rem;
 `;
 const STChatContent = styled.div`
-  clear: both;
   max-width: 100%;
   color: #202124;
   font-size: 0.8125rem;
@@ -97,5 +108,6 @@ const STChatContent = styled.div`
   padding-top: 0.625rem;
   white-space: pre-wrap;
   word-wrap: break-word;
+  margin-top: -10px;
 `;
 export default ChatBody;
